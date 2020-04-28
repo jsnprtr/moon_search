71975efaefa879be33d957fc44c945effe4e3ad8
Jason Porter <jsnprtr@gmail.com>
2020-04-28 11:21:37 +0200

    Shows the current phase of the missions in the timeline column. Parses a mission timeline based on the number of seconds. Uses the top-scrolled card to update the timeline

---
+++ src/timeline.js
@@ -0,0 +1,38 @@
+//timeline keys are seconds "up to including"
+var timeLine = {
+    702: {
+      label: "Launch"
+    },
+    9860: {
+      label: "Earth Orbit"
+    },
+    273000: {
+      label: "Earth - Moon transit (TLI)"
+    },
+    358158: {
+      label: "Lunar orbit (pre-landing)"
+    },
+    365138: {
+      label: "Eagle undocked (pre-landing)"
+    },
+    369940: {
+      label: "Lunar Landing"
+    },
+    447714: {
+      label: "On Moon"
+    },
+    460992: {
+      label: "Launch from lunar surface"
+    },
+    485801: {
+      label: "Lunar orbit (post-landing"
+    },
+    702181: {
+      label: "Moon - Earth transit (TEI)"
+    },
+    703098: {
+      label: "Re-entry and splashdown"
+    }
+  };
+
+exports.timeLine = timeLine;
